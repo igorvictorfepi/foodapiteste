31aa5638098a4cb2fa7d159283219be72fc2a5d4
Igor Victor Alves Da Silva <00019154@fepi.local>
2022-11-07 21:54:40 -0300

    commit 03

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ import databases
 import sqlalchemy
 from pydantic import BaseModel
  
-DATABASE_URL = "postgresql://jvqdzdgtbewwtv:274563f8fc0024dae7fc84b27c00cc7a593f9506775a146f2b1934bd1d5108d7@ec2-54-163-34-107.compute-1.amazonaws.com:5432/d4eq052nm6qqfd"
+DATABASE_URL = "postgresql://qvqtkcccqbhixf:07767eb05b3f44bbfea04d029da3a3ac390e7dc9122331d497c8ad1482e1c7f6@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d6qrgl98nj9om6"
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
+
+menus = sqlalchemy.Table(
+    "menus",
+    metadata,
+    sqlalchemy.Column("nome", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("img", sqlalchemy.String),
+)
 
 engine = sqlalchemy.create_engine(
     DATABASE_URL
@@ -17,29 +24,31 @@ engine = sqlalchemy.create_engine(
 
 metadata.create_all(engine)
 
-app = FastAPI()
-
-menu = sqlalchemy.Table(
-    "menu",
-    metadata,
-    sqlalchemy.Column("nome", sqlalchemy.String, primary_key=True),
-    sqlalchemy.Column("img", sqlalchemy.String),
-)
-
 class Menu(BaseModel):
     nome: str
     img: str
+
+
+app = FastAPI()
+
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
+
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
   
+
 @app.get("/menu/", response_model=List[Menu])   
 async def read_restaurantes():
-    query = menu.select()
+    query = menus.select()
     return await database.fetch_all(query) 
-    
+
+@app.post("/menu/", response_model=Menu)   
+async def create_restaurantes(menu: Menu):
+    query = menus.insert().values(nome=menu.name, img=menu.img)
+    last_record_name = await database.execute(query)
+    return {**menu.dict(), "nome": last_record_name}
