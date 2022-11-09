843a5ae8f7abd4855a5ed61bc503354088c05f6d
Igor Victor Alves Da Silva <00019154@fepi.local>
2022-11-09 20:02:32 -0300

    teste 11

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ itens = sqlalchemy.Table(
     "itens",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("subId", sqlalchemy.Integer,),
     sqlalchemy.Column("title", sqlalchemy.String),
     sqlalchemy.Column("description", sqlalchemy.String),
     sqlalchemy.Column("price", sqlalchemy.String),
@@ -61,18 +62,22 @@ class MenuIn(BaseModel):
 
 class Item(BaseModel):
     id: int
+    subId: int
     title: str
     description: str
     price: str
     image: str
 
 class ItemIn(BaseModel):
+    subId: int
     title: str
     description: str
     price: str
     image: str
 
 app = FastAPI()
+
+## INCICIO E FIM
 
 @app.on_event("startup")
 async def startup():
@@ -81,6 +86,8 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+    
+## RESTAURANTES
   
 @app.get("/menu/", response_model=List[Menu])   
 async def read_restaurantes():
@@ -93,14 +100,16 @@ async def create_restaurantes(menu: MenuIn):
     last_record_id = await database.execute(query)
     return {**menu.dict(), "id": last_record_id}
 
+## ITENS
+
 @app.get("/item/", response_model=List[Item])   
 async def read_item():
     query = itens.select()
     return await database.fetch_all(query)
 
-@app.get("/item/{id}", response_model=List[Item])   
-async def read_item_by_id(id:int):
-    query = itens.select().where(itens.c.id == id)
+@app.get("/item/{subid}", response_model=List[Item])   
+async def read_item_by_id(subid:int):
+    query = itens.select().where(itens.c.subid == subid)
     return await database.fetch_all(query)
 
 @app.post("/item/", response_model=Item)   
@@ -108,30 +117,3 @@ async def create_item(item: ItemIn):
     query = itens.insert().values(title=item.title, image=item.image, price=item.price, description=item.description)
     last_record_id2 = await database.execute(query)
     return {**item.dict(), "id": last_record_id2}
-
-
-# RESTAURANTES
-
-## 1
-@app.get("/bar1/", response_model=List[Menu])   
-async def read_restaurant():
-    query = menus.select()
-    return await database.fetch_all(query)
-
-@app.post("/bar1/", response_model=Menu)   
-async def create_restaurantes(menu: MenuIn):
-    query = menus.insert().values(nome=menu.nome, img=menu.img, preco=menu.preco, revisao=menu.revisao, avaliacao=menu.avaliacao, categoria=menu.categoria, subnome=menu.subnome)
-    last_record_id = await database.execute(query)
-    return {**menu.dict(), "id": last_record_id}
-
-## 2
-@app.get("/bar2/", response_model=List[Menu])   
-async def read_restaurant():
-    query = menus.select()
-    return await database.fetch_all(query)
-
-@app.post("/bar2/", response_model=Menu)   
-async def create_restaurantes(menu: MenuIn):
-    query = menus.insert().values(nome=menu.nome, img=menu.img, preco=menu.preco, revisao=menu.revisao, avaliacao=menu.avaliacao, categoria=menu.categoria, subnome=menu.subnome)
-    last_record_id = await database.execute(query)
-    return {**menu.dict(), "id": last_record_id}
